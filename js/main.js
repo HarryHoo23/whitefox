e945da26be2b8dc7d8ec5d85eb9f0833f98878dc
Harry Hu <hoozh1123@gmail.com>
2021-02-21 13:10:40 +1100

    add two more sections

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
     "use strict";
 
-    //initialising fullpage.js in the jQuery way
-    $('#fullpage').fullpage({
-        navigation: true,
-        slidesNavigation: true,
-    });
-
     $('#scroll-down').click(function(e){
         e.preventDefault();
         $.fn.fullpage.moveSectionDown();
@@ -16,4 +10,11 @@ $(document).ready(function(){
         $('.sunday-navbar').toggleClass("sunday-navbar-active");
     })
     
+
+    // Carousel
+
+    $('#v-pills-tab a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+      })
 });
