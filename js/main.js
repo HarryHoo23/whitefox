43d379323f2f7734e56c79a5a67bce78705a712a
Harry Hu <hoozh1123@gmail.com>
2021-02-25 21:39:54 +1100

    adjust img size

--- js/main.js
+++ js/main.js
@@ -149,56 +149,5 @@ $(document).ready(function () {
     })
 
     
-            
-
-
-
-    // showModal();
-
-    // var galleryThumbs;
-    // var galleryMain;
-
-    // var openModalBtn = $('.ss-box');
-    // openModalBtn.on('click', function (e) {
-    //     e.preventDefault();
-
-    //     openModalBtn.on('show.bs.modal', function () { 
-    //          galleryThumbs = new Swiper(".gallery-thumbs", {
-    //             centeredSlides: true,
-    //             centeredSlidesBounds: true,
-    //             slidesPerView: 3,
-    //             watchOverflow: true,
-    //             watchSlidesVisibility: true,
-    //             watchSlidesProgress: true,
-    //             direction: 'vertical'
-    //         });
-                        
-    //         galleryMain = new Swiper(".gallery-main", {
-    //         watchOverflow: true,
-    //         watchSlidesVisibility: true,
-    //         watchSlidesProgress: true,
-    //         preventInteractionOnTransition: true,
-    //         navigation: {
-    //             nextEl: '.swiper-button-next',
-    //             prevEl: '.swiper-button-prev',
-    //         },
-    //         effect: 'fade',
-    //             fadeEffect: {
-    //             crossFade: true
-    //         },
-    //         thumbs: {
-    //             swiper: galleryThumbs
-    //         }
-    //         });
-    
-    //         galleryMain.on('slideChangeTransitionStart', function() {
-    //         galleryThumbs.slideTo(galleryMain.activeIndex);
-    //         });
-    
-    //         galleryThumbs.on('transitionStart', function(){
-    //         galleryMain.slideTo(galleryThumbs.activeIndex);
-    //         });
-    //      })
-    // })      
-
+        
 });
