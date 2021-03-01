7b500bba0b32d41f0fd7a9f1624d23203acb449f
Harry Hu <hoozh1123@gmail.com>
2021-03-01 21:41:44 +1100

    add google map

--- js/main.js
+++ js/main.js
@@ -162,5 +162,164 @@ $(document).ready(function () {
           clickable: true,
         },
       });
-        
 });
+
+
+
+function initMap(lat, lng) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+               initMap(position.coords.latitude, position.coords.longitude)
+            },
+            function errorCallback(error) {
+               console.log(error)
+            }
+        );
+    }
+
+    var myLatLng = {
+       lat,
+       lng
+    };
+
+    if (myLatLng.lat !== undefined || myLatLng.lng !== undefined) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: myLatLng,
+        });
+    }
+
+    const image = 'https://firebasestorage.googleapis.com/v0/b/neat-vent-254802.appspot.com/o/marker.svg?alt=media&token=cd4f760a-2182-4e93-bf2b-3eace89f0dc1';
+ 
+    const benchMarker = new google.maps.Marker({
+       position: myLatLng,
+       map: map,
+    });
+
+    var markers = [];
+
+    for ( i = 0; i < locations.length; i++ ) {
+        const infowindow = new google.maps.InfoWindow({
+            content: locationsName[i]
+        });
+
+        const marker = new google.maps.Marker({
+            position: locations[i],
+            map,
+            icon: image,
+          });
+        markers.push(marker);
+        marker.addListener('click', () => {
+            infowindow.open(map, marker);
+        })
+    }
+
+    new MarkerClusterer(map, markers, {
+    imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+ };
+
+
+ const locations = [
+    { lat: -31.56391, lng: 147.154312 },
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 },
+    { lat: -37.759859, lng: 145.128708 },
+    { lat: -37.765015, lng: 145.133858 },
+    { lat: -37.770104, lng: 145.143299 },
+    { lat: -37.7737, lng: 145.145187 },
+    { lat: -37.774785, lng: 145.137978 },
+    { lat: -37.819616, lng: 144.968119 },
+    { lat: -38.330766, lng: 144.695692 },
+    { lat: -39.927193, lng: 175.053218 },
+    { lat: -41.330162, lng: 174.865694 },
+    { lat: -42.734358, lng: 147.439506 },
+    { lat: -42.734358, lng: 147.501315 },
+  ];
+
+var locationsName = [
+    "The Stables of Com",
+    "Hello James",
+    "Abacus Bar & Kitche",
+    "Kantee",
+    "Norman",
+    "Lucky Penny",
+    "Drugstore Espresso",
+    "Tivoli Roa",
+    "Darling Café",
+    "Einstein’s Espresso",
+    "Husband",
+    "Lawson Grov",
+    "Matild",
+    "Omni",
+    "Bar Carolin",
+    "Atlas Dinin",
+    "Gilso",
+    "France-Soi",
+    "Cucinetta",
+    "The Botanica",
+    "Entrecôt",
+    "Bistro Thierr",
+    "48h Pizza e Gnocchi",
+    "Fratellino Pizzeria",
+    "Bistro Gitan",
+    "Mopho Cantee",
+    "Thirty Eight Chair",
+    "Como Park",
+    "Fawkner Park",
+    "Royal Botanical Garden",
+    "Rockly Gardens",
+    "One Hot Yoga",
+    "REVL Training ",
+    "Prahran Aquatic Centre",
+    "Humming Puppy",
+    "F45 Toorak",
+    "Body Fit Training",
+    "GRIPT ",
+    "Bodhi & Rid",
+    "Rise Nation",
+    "Barry’s",
+    "KX Pilate",
+    "Tivoli Road Bakery",
+    "LaManna & Sons",
+    "Prahran Market",
+    "Woolworth",
+    "Toscano’s",
+    "ALDI",
+    "Coles",
+    "Rocky’s Fruit & Veg",
+    "Gum Tree Good Food",
+    "Harves",
+    "The Essential Ingredient",
+    "Simon Johnson Providor",
+    "Peter Bouchier Toora",
+    "Stocke",
+    "Chapel Street Precinct",
+    "The Como Centr",
+    "The jam Factory",
+    "Mecca Cosmetic",
+    "Toorak Villag",
+    "GRACE",
+    "Husk",
+    "GANT",
+    "Coco & Lola",
+    "Feathers",
+    "Toorak Primary School",
+    "Loreto Mandeville Hall",
+    "St Catherine’s School",
+    "St Kevin’s College",
+    "Geelong Grammar Toorak",
+    "Lauriston Girls’ School",
+    "Armadale Primary School",
+    "Christ Church Grammar School",
+    "Scotch College" 
+];
